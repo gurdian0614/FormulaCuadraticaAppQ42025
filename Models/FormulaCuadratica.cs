38a3cfaec56431242eccb1be4e13a8472f963098
gurdian0614 <ivan.gurdian@outlook.com>
2025-11-14 19:17:29 -0600

    Create Model

---
+++ Models/FormulaCuadratica.cs
@@ -0,0 +1,13 @@
+public class FormulaCuadratica
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+
+    public FormulaCuadratica(double A, double B, double C)
+    {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+    }
+}
